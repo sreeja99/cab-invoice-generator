6d82167e0ca825aa692c9595cabb77a3d39e331b
sreeja99 <srijagodishala@gmail.com>
2020-10-20 16:09:43 +0530

    Refactored:Created a Separate Method For Returning Minimum Fare

--- test/java/com/capgemini/invoiceservice/InvoiceServiceTest.java
+++ test/java/com/capgemini/invoiceservice/InvoiceServiceTest.java
@@ -13,4 +13,12 @@ public class InvoiceServiceTest {
 		double fare = invoiceGenerator.calculateFare(distance,time);
 		Assert.assertEquals(25,fare,0.0);
 	}
+	@Test
+	public void givenLessDistanceOrTimeShouldReturnMinFare() {
+		InvoiceGenerator invoiceGenerator=new InvoiceGenerator();
+		double distance=0.1;
+		int time=1;
+		double fare = invoiceGenerator.calculateFare(distance,time);
+		Assert.assertEquals(5,fare,0.0);
+	}
 }
